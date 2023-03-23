acffbb844a307ec156bdb359c59561eed7dddd13
Rsantct <rsantct@gmail.com>
2023-03-24 00:51:06 +0100

    Update grabadora_detener.sh

--- bin/grabadora_detener.sh
+++ bin/grabadora_detener.sh
@@ -6,9 +6,13 @@ if [[ $tmp ]]; then
     CARDNAME=$tmp
 fi
 
+# Paramos la grabación
 pkill -f jack_capture
+
+# Paramos el servidor de audio
 pkill -f "$CARDNAME"
 sleep 1
 
+# Desmontamos el pincho USB
 sudo umount /mnt/pinchousb 1>/dev/null 2>&1
 sync
