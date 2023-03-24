343f9aea91becf05a60295fa32682ea9649ba3de
Rsantct <rsantct@gmail.com>
2023-03-24 18:28:09 +0100

    Update and rename grabadora_botonera.py to grabadora_control.py

---
+++ bin/grabadora_control.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import  sys
+from    subprocess  import Popen
+from    gpiozero    import Button
+from    time        import sleep
+import  threading
+
+
+#################
+BOTON = Button(3)
+LED   = 'rojo'
+#################
+
+
+def _led_manager():
+    """ BUCLE infinito que gobierna un led de la placa, en función
+        del evento 'ev_blink' lo hace parpadar o lo mantiene apagado
+    """
+
+    # Por defecto usamos el LED 'rojo' de la placa
+    led = {'verde': 'led0', 'rojo': 'led1'}.get(LED, 'led1')
+
+    # BUCLE de parpadeo
+    onoff = 0
+    while True:
+
+        if ev_blink.is_set():
+            Popen(f"sudo sh -c 'echo {onoff} > /sys/class/leds/{led}/brightness'", shell=True)
+        else:
+            Popen(f"sudo sh -c 'echo {0}     > /sys/class/leds/{led}/brightness'", shell=True)
+
+        sleep(.5)
+
+        onoff = {0:1, 1:0}.get(onoff)
+
+
+def iniciar_grabacion():
+
+    print('GRABANDO')
+    # Inicia el parpadeo del LED
+    ev_blink.set()
+    # Ejecuta script de incio de la grabadora
+    # Popen(...)
+
+
+def detener_grabacion():
+
+    print('PARANDO')
+    # Detiene el parpadeo del LED
+    ev_blink.clear()
+    # Ejecuta script de detención de la grabadora
+    # Popen(...)
+
+
+def _boton_lector(tg=3):
+    """ BUCLE INFINITO que lee las pulsaciones del botón de control,
+        con un tiempo de guarda tg (por defecto 3 segundos) hasta empezar
+        a leer de nuevo.
+    """
+
+    def espera_pulsacion(t=3):
+        """ Comprueba que el botón esté pulsado de forma sostenida
+            durante t segundos
+        """
+        BOTON.wait_for_press()
+
+    grabar = False
+    while True:
+        espera_pulsacion()
+        grabar = {True:False, False:True}.get(grabar)
+        if grabar:
+            iniciar_grabacion()
+        else:
+            detener_grabacion()
+        sleep(tg)
+
+
+if __name__ == "__main__":
+
+    # Inicia la gestión del LED
+    ev_blink = threading.Event()
+    th_led = threading.Thread(target=_led_manager)
+    th_led.start()
+
+
+    # Inicia la lectura del botón de control
+    print('PULSAR BOTON PARA INICIAR/PARAR')
+    th_boton = threading.Thread(target=_boton_lector)
+    th_boton.start()
+
