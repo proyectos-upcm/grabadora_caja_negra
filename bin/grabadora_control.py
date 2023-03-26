bb172a143c5b98d827402bd5740a1bc19a7936e5
Rsantct <rsantct@gmail.com>
2023-03-26 01:00:16 +0100

    Update grabadora_control.py

--- bin/grabadora_control.py
+++ bin/grabadora_control.py
@@ -4,19 +4,24 @@
 """
 
 import  subprocess  as sp
-from    gpiozero    import Button
+import  gpiozero
 from    time        import sleep
 import  threading
 
 
 ########################################################################
 # GPIO-03_pin-05 <---> GND_pin-06 (tercera pareja de pines)
-BOTON = Button(3) # GPIO-03
-LED   = 'rojo'
+BOTON = gpiozero.Button(3)  # GPIO-03
+
+# Opción con LED integrado en placa (rojo o verde)
+#LED   = 'rojo'
+
+# Opción con LED dedidcado en un pin GPIO
+LED   = gpiozero.LED(15)    # GPIO-15 (quinta pareja de pines)
 ########################################################################
 
 
-def _bucle_led():
+def _bucle_led_placa():
     """ BUCLE INFINITO que gobierna un led de la placa, en función
         del evento 'ev_blink' lo hace parpadar o lo mantiene apagado
     """
@@ -40,6 +45,7 @@ def _bucle_led():
     while True:
 
         if ev_blink.is_set():
+            # Parpadeo normal cuando de detecte jack_capture funcionando
             if not jack_capture:
                 blink('rapido')
                 try:
@@ -84,32 +90,71 @@ def _bucle_boton(tp=3):
             detener_grabacion()
 
 
+def led_blink(mode='normal'):
+    """ Usado para gestionar un LED dedicado en un pin GPIO
+    """
+
+    if mode == 'normal':
+        ton = 1.5; toff = 1.0
+    elif mode == 'rapido':
+        ton = 0.15; toff = 0.15
+    else:
+        LED.off()
+        return
+
+    LED.blink(on_time=ton, off_time=toff)
+
+
 def iniciar_grabacion():
 
     print('GRABANDO')
-    # Inicia el parpadeo del LED
-    ev_blink.set()
     # Ejecuta script de incio de la grabadora
     sp.Popen("~/bin/grabadora_iniciar.sh", shell=True)
+
+    # Inicia el parpadeo del LED
+    if led_integrado:
+
+        ev_blink.set()
+
+    else:
+
+        led_blink('rapido')
+
+        # Parpadeo normal cuando de detecte jack_capture funcionando
+        jack_capture = False
+        c=10
+        while (c or not jack_capture):
+            try:
+                jack_capture = sp.check_output('pgrep -f jack_capture'.split())
+                led_blink('normal')
+            except:
+                pass
+            c -=1
+            sleep(.5)
+
 
 
 def detener_grabacion():
 
     print('PARANDO')
-    # Detiene el parpadeo del LED
-    ev_blink.clear()
     # Ejecuta script de detención de la grabadora
     sp.Popen("~/bin/grabadora_detener.sh", shell=True)
+    # Detiene el parpadeo del LED
+    if led_integrado:
+        ev_blink.clear()
+    else:
+        led_blink('off')
 
 
 if __name__ == "__main__":
-
-    # Inicia la gestión del LED
-    ev_blink = threading.Event()
-    th_led = threading.Thread(target=_bucle_led)
-    th_led.start()
 
     # Inicia la lectura del botón de control
     th_boton = threading.Thread(target=_bucle_boton)
     th_boton.start()
 
+    # Inicia la gestión del LED integrado en placa si es el elegido
+    led_integrado = True if (type(LED) == str) else False
+    if led_integrado:
+        ev_blink = threading.Event()
+        th_led = threading.Thread(target=_bucle_led_placa)
+        th_led.start()
